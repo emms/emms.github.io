6b37e7ae3931d5d4bb75363dbbbf5c72120215df
Emmi Linkola <emmi.linkola@aalto.fi>
2016-06-11 16:58:36 +0300

    Junction responsiveness update

--- Junction16/js/app/calculateTrackBoxHeights.js
+++ Junction16/js/app/calculateTrackBoxHeights.js
@@ -1,13 +1,16 @@
 function calculateTrackBoxHeights() {
-    var box1height = parseFloat($(".track-section").css("width")) / 4 - 20 + "px";
-    var boxes = [];
-    $('.box-1-height').css('height', box1height);
-    $('.box-1-width').css('width', box1height);
-    for (var i = 1; i < 5; ++i) {
-        boxes[i] = parseFloat(box1height) * (i + 1) + i * 6 + 'px';
-        var selector = '.box-' + (i + 1) + '-height';
-        var selector2 = '.box-' + (i + 1) + '-width';
-        $(selector).css("height", boxes[i]);
-        $(selector2).css("width", boxes[i]);
+    if (window.innerWidth > 768) {
+        var box1height = parseFloat($(".track-section").css("width")) / 4 - 20 + "px";
+        var boxes = [];
+        $('.box-1-height').css('height', box1height);
+        $('.box-1-5-height').css('height', parseFloat(box1height) * 1.5 + 'px');
+        $('.box-1-width').css('width', box1height);
+        for (var i = 1; i < 5; ++i) {
+            boxes[i] = parseFloat(box1height) * (i + 1) + i * 10 + 'px';
+            var selector = '.box-' + (i + 1) + '-height';
+            var selector2 = '.box-' + (i + 1) + '-width';
+            $(selector).css("height", boxes[i]);
+            $(selector2).css("width", boxes[i]);
+        }
     }
 }
