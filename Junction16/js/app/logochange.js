9438a5c96e75fe94c0781627b304e399fbc67329
Emmi Linkola <emmi.linkola@aalto.fi>
2016-06-11 17:17:03 +0300

    Junction mobile style update

--- Junction16/js/app/logochange.js
+++ Junction16/js/app/logochange.js
@@ -1,28 +1,29 @@
-$(".main-logo").hover(
-    function() {
-        //if ($(".main-logo").css('opacity') === 1) {
-        $(this).animate({
-            opacity: 0
-        });
-        $('.buzz_wrapper').animate({
-            opacity: 1
-        });
-
-        //}
-    },
-    function() {
-        //if ($(":animated").length === 0) {
-        $('.buzz_wrapper').animate({
-            opacity: 0
-        }).promise().done(function() {
-            $(".main-logo").animate({
+if (window.innerWidth > 480) {
+    $(".main-logo").hover(
+        function() {
+            //if ($(".main-logo").css('opacity') === 1) {
+            $(this).animate({
+                opacity: 0
+            });
+            $('.buzz_wrapper').animate({
                 opacity: 1
             });
-        });
-        //}
-    }
-);
 
+            //}
+        },
+        function() {
+            //if ($(":animated").length === 0) {
+            $('.buzz_wrapper').animate({
+                opacity: 0
+            }).promise().done(function() {
+                $(".main-logo").animate({
+                    opacity: 1
+                });
+            });
+            //}
+        }
+    );
+}
 /*
 $(".buzz_wrapper").hover(
     function() {
