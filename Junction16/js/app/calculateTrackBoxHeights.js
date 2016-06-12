d1129cdaf247070739069699258119c516773b41
Emmi Linkola <emmi.linkola@aalto.fi>
2016-06-12 16:36:08 +0300

    Junction update

--- Junction16/js/app/calculateTrackBoxHeights.js
+++ Junction16/js/app/calculateTrackBoxHeights.js
@@ -1,12 +1,12 @@
 function calculateTrackBoxHeights() {
     if (window.innerWidth > 768) {
-        var box1height = parseFloat($(".track-section").css("width")) / 4 - 20 + "px";
+        var box1height = parseFloat($(".track-section").css("width")) / 4 - 30 + "px";
         var boxes = [];
         $('.box-1-height').css('height', box1height);
         $('.box-1-5-height').css('height', parseFloat(box1height) * 1.5 + 'px');
         $('.box-1-width').css('width', box1height);
         for (var i = 1; i < 5; ++i) {
-            boxes[i] = parseFloat(box1height) * (i + 1) + i * 10 + 'px';
+            boxes[i] = parseFloat(box1height) * (i + 1) + i * 15 + 'px';
             var selector = '.box-' + (i + 1) + '-height';
             var selector2 = '.box-' + (i + 1) + '-width';
             $(selector).css("height", boxes[i]);
